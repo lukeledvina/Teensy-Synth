99a52a96751c0f1e6d5f8298ced023dcecd26e66
Luke Ledvina <lukeledvina19@gmail.com>
2025-05-16 12:18:46 -0400

    Add first functional nested menu

--- Teensy-Synth.ino
+++ Teensy-Synth.ino
@@ -9,11 +9,27 @@
 #define OLED_RESET -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-const char* mainMenuItems[] = {"Osc A", "Osc B", "Filter", "Envelopes"};
+enum menuState {
+  MAIN,
+  OSC_A,
+  OSC_B,
+  FILTER,
+  AMP_ENV,
+  FILTER_ENV
+};
+
+menuState currentMenu = MAIN;
+
+const char* mainMenuItems[] = {"Osc A", "Osc B", "Amp Env", "Filter", "Filter Env"};
 int mainMenuIndex = 0;
 const int mainMenuLength = sizeof(mainMenuItems) / sizeof(mainMenuItems[0]); // Gets length by taking the memory size of each
 int mainPageNumber = 0;
 const int itemsPerPage = 3;
+
+const char* oscAItems[] = {"Waveform", "Pitch", "Pulse Width", "Osc On/Off"};
+int oscAMenuIndex = 0;
+const int oscAMenuLength = sizeof(oscAItems) / sizeof(oscAItems[0]);
+int oscAPageNumber = 0;
 
 
 
@@ -33,6 +49,108 @@ void setup() {
   display.setTextSize(2);
 }
 
+// needs to be aware of proper menu and update everything accordingly
+void forward() {
+  switch (currentMenu) {
+    case MAIN:
+      mainMenuIndex = (mainMenuIndex + 1) % mainMenuLength;
+      updateMenu(mainPageNumber, mainMenuIndex, mainMenuLength, mainMenuItems);
+      Serial.println(mainMenuIndex);
+      break;
+    
+    case OSC_A:
+      oscAMenuIndex = (oscAMenuIndex + 1) % oscAMenuLength;
+      updateMenu(oscAPageNumber, oscAMenuIndex, oscAMenuLength, oscAItems);
+      Serial.println(oscAMenuIndex);
+      break;
+
+    case OSC_B:
+
+      break;
+
+    case FILTER:
+
+      break;
+
+    case AMP_ENV:
+
+      break;
+
+    case FILTER_ENV:
+
+      break;
+  }
+}
+
+// same as forward
+void backward() {
+  switch (currentMenu) {
+    case MAIN:
+      mainMenuIndex = (mainMenuIndex - 1) % mainMenuLength;
+      updateMenu(mainPageNumber, mainMenuIndex, mainMenuLength, mainMenuItems);
+      break;
+    
+    case OSC_A:
+      oscAMenuIndex = (oscAMenuIndex - 1) % oscAMenuLength;
+      updateMenu(oscAPageNumber, oscAMenuIndex, oscAMenuLength, oscAItems);
+      break;
+
+    case OSC_B:
+
+      break;
+
+    case FILTER:
+
+      break;
+
+    case AMP_ENV:
+
+      break;
+
+    case FILTER_ENV:
+
+      break;
+  }
+}
+
+void select() {
+  switch (currentMenu) {
+    case MAIN:
+      if (mainMenuIndex == 0) {
+        // Osc A
+        display.setCursor(0,0);
+        display.setTextColor(SSD1306_WHITE); // Reset text color so that when selecting options of multiples of 3, doesn't keep them highlighted
+        updateMenu(oscAPageNumber, oscAMenuIndex, oscAMenuLength, oscAItems);
+        currentMenu = OSC_A;
+
+      }
+
+      break;
+
+    case OSC_A:
+
+      break;
+
+    case OSC_B:
+
+      break;
+
+    case FILTER:
+
+      break;
+
+    case AMP_ENV:
+
+      break;
+
+    case FILTER_ENV:
+
+      break;
+  }
+
+
+}
+
 void loop() {
   long newEncoderPosition = myEncoder.read();
   long positionChange = newEncoderPosition - lastEncoderPosition;
@@ -40,29 +158,30 @@ void loop() {
   // code needs to be changed such that depending on if you are in a menu or in a parameter it properly updates that.
   if (positionChange >= 4) {
     lastEncoderPosition = newEncoderPosition;
-    mainMenuIndex = (mainMenuIndex + 1) % mainMenuLength;
-    updateMenu(mainPageNumber, mainMenuIndex, mainMenuLength, mainMenuItems);
+    // implement "forward" function
+    forward();
   } else if (positionChange <= -4) {
     lastEncoderPosition = newEncoderPosition;
-    mainMenuIndex = (mainMenuIndex - 1 + mainMenuLength) % mainMenuLength;
-    updateMenu(mainPageNumber, mainMenuIndex, mainMenuLength, mainMenuItems);
+    // Implement "backward" function
+    backward();
   }
 
   if (digitalRead(encoderSwitch) == LOW) {
-    display.clearDisplay();
-    display.setTextColor(SSD1306_WHITE); // Reset text color so that when selecting options of multiples of 3, doesn't keep them highlighted
-    display.setCursor(0,0);
-    display.print("Selected: ");
-    display.println(mainMenuItems[mainMenuIndex]);
-    display.display();
-    delay(500);
+    
+    
+    // Implement "select" function
+    select();
     while(digitalRead(encoderSwitch) == LOW);
   }
+
+  // Add back button functionality
+
 }
 
 // refactor this to work with more menus, instead of hardcoding it to work with the main menu, get it to work with submenus based on input parameters
 void updateMenu(int pageNumber, int menuIndex, int menuLength, const char* menuItems[]) {
   display.clearDisplay();
+  
 
   pageNumber = menuIndex/3;
   int totalPages = (menuLength + itemsPerPage - 1) / itemsPerPage;
