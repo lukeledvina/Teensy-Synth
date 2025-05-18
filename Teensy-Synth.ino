c643561cc625f6eb775cc87ab36c7320510c4595
Luke Ledvina <lukeledvina19@gmail.com>
2025-05-18 17:02:42 -0400

    Add full filter functionality

--- Teensy-Synth.ino
+++ Teensy-Synth.ino
@@ -16,16 +16,24 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 // GUItool: begin automatically generated code
-AudioSynthWaveform       waveformA;      //xy=186.23333740234375,264
-AudioMixer4              mixerRight;         //xy=417.23333740234375,348.23333740234375
-AudioMixer4              mixerLeft;         //xy=422.23333740234375,228.23333740234375
-AudioOutputI2S           i2s1;           //xy=674.2333374023438,257.23333740234375
-AudioConnection          patchCord1(waveformA, 0, mixerLeft, 0);
-AudioConnection          patchCord2(waveformA, 0, mixerRight, 0);
-AudioConnection          patchCord3(mixerRight, 0, i2s1, 1);
-AudioConnection          patchCord4(mixerLeft, 0, i2s1, 0);
-AudioControlSGTL5000     sgtl5000_1;     //xy=420.23333740234375,466.23333740234375
+AudioSynthWaveform       waveformA;      //xy=69.23333740234375,220
+AudioFilterStateVariable filter1;        //xy=220.23333740234375,145.23333740234375
+AudioMixer4              filterTypeMixer;         //xy=341.23333740234375,262.23333740234375
+AudioMixer4              mixerLeft;         //xy=562.2333374023438,164.23333740234375
+AudioMixer4              mixerRight;         //xy=590.2333374023438,348.23333740234375
+AudioOutputI2S           i2s1;           //xy=718.2333374023438,254.23333740234375
+AudioConnection          patchCord1(waveformA, 0, filter1, 0);
+AudioConnection          patchCord2(waveformA, 0, filterTypeMixer, 3);
+AudioConnection          patchCord3(filter1, 0, filterTypeMixer, 0);
+AudioConnection          patchCord4(filter1, 1, filterTypeMixer, 1);
+AudioConnection          patchCord5(filter1, 2, filterTypeMixer, 2);
+AudioConnection          patchCord6(filterTypeMixer, 0, mixerLeft, 0);
+AudioConnection          patchCord7(filterTypeMixer, 0, mixerRight, 0);
+AudioConnection          patchCord8(mixerLeft, 0, i2s1, 0);
+AudioConnection          patchCord9(mixerRight, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=377.23333740234375,658.2333374023438
 // GUItool: end automatically generated code
+
 
 
 
@@ -34,7 +42,11 @@ enum menuState {
   MAIN,
   OSC_A,
   // OSC_B,
-  // FILTER,
+  FILTER,
+  FILTER_TYPE,
+  FILTER_CUTOFF,
+  FILTER_RESONANCE,
+  FILTER_ON_OFF,
   // AMP_ENV,
   // FILTER_ENV,
   WAVEFORM_A,
@@ -45,7 +57,7 @@ enum menuState {
 
 menuState currentMenu = MAIN;
 
-const char* mainMenuItems[] = {"Osc A", "Osc B", "Amp Env", "Filter", "Filter Env"};
+const char* mainMenuItems[] = {"Osc A", "Osc B", "Filter", "Amp Env", "Filter Env", "Volume", "Mono/Poly"};
 int mainMenuIndex = 0;
 const int mainMenuLength = sizeof(mainMenuItems) / 4;
 int mainMenuPageNumber = 0;
@@ -60,6 +72,16 @@ const char* waveformMenuItems[] = {"Sine", "Sawtooth", "Square", "Triangle", "Pu
 int waveformMenuIndex = 0;
 const int waveformMenuLength = sizeof(waveformMenuItems) / 4;
 int waveformMenuPageNumber = 0;
+
+const char* filterMenuItems[] = {"Type", "Cutoff", "Resonance", "Ftr On/Off"};
+int filterMenuIndex = 0;
+const int filterMenuLength = sizeof(filterMenuItems) / 4;
+int filterMenuPageNumber = 0;
+
+const char* filterTypeMenuItems[] = {"Low Pass", "Band Pass", "High Pass"};
+int filterTypeMenuIndex = 0;
+const int filterTypeMenuLength = sizeof(filterTypeMenuItems) / 4;
+int filterTypeMenuPageNumber = 0;
 
 int oscAPitchOffset = 0;
 float oscAPulseWidth = 0.5f;
@@ -79,6 +101,28 @@ int oscAWaveform = WAVEFORM_SINE;
 float oscAGainLeft = 0.5f;
 float oscAGainRight = 0.5f;
 float volume = 0.5f;
+
+int minCutoffFreq = 20;
+int maxCutoffFreq = 15000;
+int filterCutoffFreq = maxCutoffFreq;
+float filterControl = 1.0f;
+float filterCutoffCurve = 3.0f;
+
+float filterResonance = 0.0f;
+float minResonance = 0.7f;
+float maxResonance = 5.0f;
+
+bool filterOn = false;
+
+enum filterType {
+  LOW_PASS,
+  BAND_PASS,
+  HIGH_PASS
+};
+
+filterType currentFilterType = LOW_PASS;
+
+
 
 void setup() {
 
@@ -101,10 +145,15 @@ void setup() {
   display.setTextSize(2);
   updateMenu(mainMenuPageNumber, mainMenuIndex, mainMenuLength, mainMenuItems);
 
-  waveformA.begin(WAVEFORM_SINE);
+  waveformA.begin(WAVEFORM_PULSE);
 
   usbMIDI.setHandleNoteOn(onNoteOn);
   usbMIDI.setHandleNoteOff(onNoteOff);
+
+  filterTypeMixer.gain(0, 0);
+  filterTypeMixer.gain(1, 0);
+  filterTypeMixer.gain(2, 0);
+  filterTypeMixer.gain(3, 1);
 }
 
 // needs to be aware of proper menu and update everything accordingly
@@ -138,7 +187,7 @@ void forward() {
 
     case PULSE_WIDTH_A:
       oscAPulseWidth = min(oscAPulseWidth + 0.02f, 1.00f);
-      //waveformA.pulseWidth(oscAPulseWidth);
+      waveformA.pulseWidth(oscAPulseWidth);
       display.clearDisplay();
       display.setCursor(0, 0);
       display.setTextColor(SSD1306_WHITE);
@@ -156,9 +205,29 @@ void forward() {
 
     //   break;
 
-    // case FILTER:
+    case FILTER:
+      filterMenuIndex = (filterMenuIndex + 1) % filterMenuLength;
+      updateMenu(filterMenuPageNumber, filterMenuIndex, filterMenuLength, filterMenuItems);
+      break;
 
-    //   break;
+    case FILTER_TYPE:
+      filterTypeMenuIndex = (filterTypeMenuIndex + 1) % filterTypeMenuLength;
+      updateMenu(filterTypeMenuPageNumber, filterTypeMenuIndex, filterTypeMenuLength, filterTypeMenuItems);
+      break;
+
+    case FILTER_CUTOFF:
+      filterControl = min(filterControl + 0.01f, 1.0f);
+      applyFilterCutoff();
+      break;
+
+    case FILTER_RESONANCE:
+      filterResonance = min(1.0f, filterResonance + 0.02f);
+      applyFilterResonance();
+      break;
+
+    case FILTER_ON_OFF:
+      // do nothing
+      break;
 
     // case AMP_ENV:
 
@@ -201,7 +270,7 @@ void backward() {
 
     case PULSE_WIDTH_A:
       oscAPulseWidth = max(oscAPulseWidth - 0.02f, 0.00f);
-      //waveformA.pulseWidth(oscAPulseWidth);
+      waveformA.pulseWidth(oscAPulseWidth);
       display.clearDisplay();
       display.setCursor(0, 0);
       display.setTextColor(SSD1306_WHITE);
@@ -212,16 +281,36 @@ void backward() {
       break;
 
     case OSC_A_ON_OFF:
-
+      // do nothing
       break;
 
     // case OSC_B:
 
     //   break;
 
-    // case FILTER:
+    case FILTER:
+      filterMenuIndex = (filterMenuIndex - 1) % filterMenuLength;
+      updateMenu(filterMenuPageNumber, filterMenuIndex, filterMenuLength, filterMenuItems);
+      break;
 
-    //   break;
+    case FILTER_TYPE:
+      filterTypeMenuIndex = (filterTypeMenuIndex - 1) % filterTypeMenuLength;
+      updateMenu(filterTypeMenuPageNumber, filterTypeMenuIndex, filterTypeMenuLength, filterTypeMenuItems);
+      break;
+
+    case FILTER_CUTOFF:
+      filterControl = max(filterControl - 0.01f, 0.0f);
+      applyFilterCutoff();
+      break;
+
+    case FILTER_RESONANCE:
+      filterResonance = max(0.0f, filterResonance - 0.02f);
+      applyFilterResonance();
+      break;
+
+    case FILTER_ON_OFF:
+      // do nothing
+      break;
 
     // case AMP_ENV:
 
@@ -243,6 +332,14 @@ void select() {
         oscAMenuIndex = 0;
         display.setCursor(0,0);
         updateMenu(oscAMenuPageNumber, oscAMenuIndex, oscAMenuLength, oscAMenuItems);
+      } else if (mainMenuIndex == 1) {
+
+      } else if (mainMenuIndex == 2) {
+        currentMenu = FILTER;
+        filterMenuPageNumber = 0;
+        filterMenuIndex = 0;
+        display.setCursor(0,0);
+        updateMenu(filterMenuPageNumber, filterMenuIndex, filterMenuLength, filterMenuItems);
       }
 
       break;
@@ -292,7 +389,6 @@ void select() {
       }
       break;
 
-// works, just need it to actually alter the waveforms now
     case WAVEFORM_A:
       display.clearDisplay();
       display.setCursor(0, 0);
@@ -335,9 +431,104 @@ void select() {
 
     //   break;
 
-    // case FILTER:
+    case FILTER:
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.setTextColor(SSD1306_WHITE);
+      if (filterMenuIndex == 0) {
+        //filter type
+        currentMenu = FILTER_TYPE;
+        filterTypeMenuPageNumber = 0;
+        filterTypeMenuIndex = 0;
+        display.setCursor(0,0);
+        updateMenu(filterTypeMenuPageNumber, filterTypeMenuIndex, filterTypeMenuLength, filterTypeMenuItems);
+      } else if (filterMenuIndex == 1) {
+        //filter cutoff
+        currentMenu = FILTER_CUTOFF;
+        applyFilterCutoff();
+      } else if (filterMenuIndex == 2) {
+        //filter resonance
+        currentMenu = FILTER_RESONANCE;
+        applyFilterResonance();
+      } else if (filterMenuIndex == 3) {
+        filterOn = !filterOn;
+        Serial.println(filterCutoffFreq);
+        if (filterOn) {
+          display.println("Filter On");
+          filter1.frequency(filterCutoffFreq);
+          switch (currentFilterType) {
+            case LOW_PASS:
+              filterTypeMixer.gain(0, 1);
+              filterTypeMixer.gain(1, 0);
+              filterTypeMixer.gain(2, 0);
+              filterTypeMixer.gain(3, 0);
+              break;
 
-    //   break;
+            case BAND_PASS:
+              filterTypeMixer.gain(0, 0);
+              filterTypeMixer.gain(1, 1);
+              filterTypeMixer.gain(2, 0);
+              filterTypeMixer.gain(3, 0);
+              break;
+
+            case HIGH_PASS:
+              filterTypeMixer.gain(0, 0);
+              filterTypeMixer.gain(1, 0);
+              filterTypeMixer.gain(2, 1);  
+              filterTypeMixer.gain(3, 0);                 
+              break;       
+          }
+
+        } else {
+          display.println("Filter Off");
+          filterTypeMixer.gain(0, 0);
+          filterTypeMixer.gain(1, 0);
+          filterTypeMixer.gain(2, 0);
+          filterTypeMixer.gain(3, 1);
+        }
+      }
+      display.display();
+      break;
+
+    case FILTER_TYPE:
+      display.clearDisplay();
+      display.setCursor(0, 0);
+      display.setTextColor(SSD1306_WHITE);
+      display.println("Selected: ");
+      // filter1.frequency(filterCutoffFreq);
+      if (filterTypeMenuIndex == 0) {
+        currentFilterType = LOW_PASS;
+        display.println("Low Pass");
+        filterTypeMixer.gain(0, 1);
+        filterTypeMixer.gain(1, 0);
+        filterTypeMixer.gain(2, 0);
+      } else if (filterTypeMenuIndex == 1) {
+        currentFilterType = BAND_PASS;
+        display.println("Band Pass");
+        filterTypeMixer.gain(0, 0);
+        filterTypeMixer.gain(1, 1);
+        filterTypeMixer.gain(2, 0);
+      } else if (filterTypeMenuIndex == 2) {
+        currentFilterType = HIGH_PASS;
+        display.println("High Pass");
+        filterTypeMixer.gain(0, 0);
+        filterTypeMixer.gain(1, 0);
+        filterTypeMixer.gain(2, 1);
+      }
+      display.display();
+      break;
+
+    case FILTER_CUTOFF:
+      // do nothing
+      break;
+
+    case FILTER_RESONANCE:
+      // do nothing
+      break;
+
+    case FILTER_ON_OFF:
+      // do nothing
+      break;
 
     // case AMP_ENV:
 
@@ -394,9 +585,40 @@ void goBack() {
 
     //   break;
 
-    // case FILTER:
+    case FILTER:
+      currentMenu = MAIN;
+      mainMenuIndex = 0;
+      mainMenuPageNumber = 0;
+      updateMenu(mainMenuPageNumber, mainMenuIndex, mainMenuLength, mainMenuItems);
+      break;
 
-    //   break;
+    case FILTER_TYPE:
+      currentMenu = FILTER;
+      filterMenuIndex = 0;
+      filterMenuPageNumber = 0;
+      updateMenu(filterMenuPageNumber, filterMenuIndex, filterMenuLength, filterMenuItems);
+      break;
+
+    case FILTER_CUTOFF:
+      currentMenu = FILTER;
+      filterMenuIndex = 0;
+      filterMenuPageNumber = 0;
+      updateMenu(filterMenuPageNumber, filterMenuIndex, filterMenuLength, filterMenuItems);
+      break;
+
+    case FILTER_RESONANCE:
+      currentMenu = FILTER;
+      filterMenuIndex = 0;
+      filterMenuPageNumber = 0;
+      updateMenu(filterMenuPageNumber, filterMenuIndex, filterMenuLength, filterMenuItems);
+      break;
+
+    case FILTER_ON_OFF:
+      currentMenu = FILTER;
+      filterMenuIndex = 0;
+      filterMenuPageNumber = 0;
+      updateMenu(filterMenuPageNumber, filterMenuIndex, filterMenuLength, filterMenuItems);
+      break;
 
     // case AMP_ENV:
 
@@ -470,3 +692,32 @@ void onNoteOff(byte channel, byte note, byte velocity) {
   waveformA.amplitude(0);
 }
 
+
+void applyFilterCutoff() {
+  float filterCurve = pow(filterControl, filterCutoffCurve);
+  filterCutoffFreq = minCutoffFreq * pow(maxCutoffFreq / minCutoffFreq, filterCurve);
+
+  filter1.frequency(filterCutoffFreq);
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextColor(SSD1306_WHITE);
+  display.println("Cutoff: ");
+  display.print((int)filterCutoffFreq);
+  display.println(" Hz");
+  display.display();
+}
+
+void applyFilterResonance() {
+
+  float adjustedResonance = minResonance + (maxResonance - minResonance) * filterResonance;
+
+  filter1.resonance(adjustedResonance);
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextColor(SSD1306_WHITE);
+  display.println("Resonance:");
+  display.print(filterResonance * 100);
+  display.println(" %");
+  display.display();
+}
